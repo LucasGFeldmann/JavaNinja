234e710cac89d3334b8c5bf093c4bd196f752bbe
LucasGFeldmann <lucasgfeldmann@gmail.com>
2025-02-04 07:52:22 -0500

    Initial commit

---
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,9 @@
+package TiposDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+        String nome = "Vitoria";
+       String nomeUpperCase = nome.toUpperCase();
+        System.out.println(nomeUpperCase);
+    }
+}
---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,16 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        int idade = 2147483647; // valor maximo 2147483647
+        double altura = 1.85;
+        char inicial = 'L';
+        boolean vivoOuMorto = true;
+        Long saldobancario = 999999999L;
+
+        System.out.println(idade);
+        System.out.println("saldobancario = " + saldobancario);
+        System.out.println("Minha idade é: " + idade);
+
+    }
+}
