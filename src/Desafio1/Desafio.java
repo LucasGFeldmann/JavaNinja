a14279868714ad2652f8ffe0cc8681a5762a6d23
LucasGFeldmann <lucasgfeldmann@gmail.com>
2025-02-04 14:13:15 -0300

    concluido: Desafio1

--- src/Desafio1/Desafio.java
+++ src/Desafio1/Desafio.java
@@ -1,9 +1,31 @@
 package Desafio1;
 
+
 public class Desafio {
+    public static void ninja(String nome, short idade, String missao, char nivelDificuldade, String status) {
+        System.out.println("----------------------------------------");
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(missao);
+        System.out.println(nivelDificuldade);
+        System.out.println(status);
+
+        if (idade < 15) {
+            if (nivelDificuldade == 'C' || nivelDificuldade == 'D') {
+                status = "Missão Concluida!";
+            } else {
+                status = "Missão não concluida, idade inferior";
+            }
+        } else {
+            status = "Missão Não Concluida";
+        }
+        System.out.println(status);
+        System.out.println("----------------------------------------");
+    }
     public static void main(String[] args) {
-        String nome = "Lucas";
-        short idade = 20;
+        ninja("Lucas", (short) 20,"Aprender Java",'A',"Em andamento");
+        ninja("Evandro", (short) 14,"Aprender Java",'A',"Em andamento");
+        ninja("Erike", (short) 14,"Aprender Java",'C',"Em andamento");
 
     }
 }
